2fa6bfd7dc55986b905d539da8208da9d156149f
rukubaruku <vogliarmant@gmail.com>
2025-07-30 10:04:33 +0200

    changing port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3107;
 
 app.use(
   cors({
